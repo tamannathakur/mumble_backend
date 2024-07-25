2446d3afe912b591f7cc732ea7bd76658d846dd5
tamannathakur <tamannathakur2710@gmail.com>
2024-07-26 03:27:43 +0530

    commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 const dbUri = process.env.MONGODB_URI;
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbUri)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
